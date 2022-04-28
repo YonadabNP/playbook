d12522b99cbe8032da7b3f95b3750cf323f72d53
YonadabNP <99059719+YonadabNP@users.noreply.github.com>
2022-04-27 21:26:48 -0500

    add: files for practice 1

---
+++ weekly_mission_4/libs/services/FizzbuzzService.js
@@ -0,0 +1,28 @@
+class FizzbuzzService {
+  constructor() {}
+
+  getInfo() {
+    return `Hey there!`;
+  }
+
+  static applyValidationInExplorer(explorer) {
+    if(explorer.score%5 === 0 && explorer.score%3 === 0){
+        explorer.trick = "FIZZBUZZ";
+        return explorer;
+    }
+    else if(explorer.score%3 === 0){
+        explorer.trick = "FIZZ";
+        return explorer;
+    }
+    else if(explorer.score%5 === 0){
+        explorer.trick = "BUZZ";
+        return explorer;
+    }
+    else{
+        explorer.trick = explorer.score;
+        return explorer;
+    }
+  }
+}
+
+module.exports = FizzbuzzService;
---
+++ weekly_mission_4/test/libs/services/FizzbuzzService.test.js
@@ -0,0 +1,28 @@
+const Reader = require("./../../../libs/utils/Reader");
+const FizzbuzzService = require("./../../../libs/services/FizzbuzzService");
+
+describe("Test ExplorerService.js", () => {
+  test("Fizzbuzz Explorer 1", () => {
+    const explorer1 = { name: "Explorer1", score: 1 };
+    FizzbuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1}
+    expect(explorer1.trick).toBe(1);
+  });
+
+  test("Fizzbuzz explorer3", () => {
+    const explorer3 = { name: "Explorer3", score: 3 };
+    FizzbuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+    expect(explorer3.trick).toBe("FIZZ");
+  });
+
+  test("Fizzbuzz explorer5", () => {
+    const explorer5 = { name: "Explorer5", score: 5 };
+    FizzbuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+    expect(explorer5.trick).toBe("BUZZ");
+  });
+
+  test("Fizzbuzz explorer15", () => {
+    const explorer15 = { name: "Explorer15", score: 15 };
+    FizzbuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+    expect(explorer15.trick).toBe("FIZZBUZZ");
+  });
+});
