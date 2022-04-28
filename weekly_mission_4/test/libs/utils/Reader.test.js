13ce101f9ee15e09d05b5bd1aadacec4cbf95fc5
YonadabNP <99059719+YonadabNP@users.noreply.github.com>
2022-04-27 21:59:33 -0500

    delete: removing extra files

--- weekly_mission_4/test/libs/utils/Reader.test.js
+++
@@ -1,8 +0,0 @@
-const Reader = require("./../../../libs/utils/Reader");
-
-describe("Test Reader.js", () => {
-    test('Return', () => {
-        const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
-      expect(explorers.length).toBe(15);
-    });
-  });
